71e71d9017fc498a566955729d1af4c8c1a8835d
William Coates <will.t.coates@gmail.com>
2020-09-10 17:23:35 +0100

    Initial implementation

---
+++ src/background.js
@@ -0,0 +1,47 @@
+const specifiers = {
+    '%': () => '%',
+    'y': item => new Date(item.startTime).getFullYear().toString(),
+    'm': item => (new Date(item.startTime).getMonth() + 1).toString().padStart(2, '0'),
+    'd': item => new Date(item.startTime).getDate().toString().padStart(2, '0'),
+    'h': item => new Date(item.startTime).getHours().toString().padStart(2, '0'),
+    'M': item => new Date(item.startTime).getMinutes().toString().padStart(2, '0'),
+    's': item => new Date(item.startTime).getSeconds().toString().padStart(2, '0'),
+    'f': item => item.filename
+}
+
+function formatFilename(downloadItem, format) {
+    let filename = "";
+
+    for (let last = 0, next = 0; next != -1; last = next + 2) {
+        next = format.indexOf('%', last);
+        filename += format.substring(last, next == -1 ? undefined : next);
+        if (next != -1) {
+            let char = format.charAt(next + 1);
+            if (char == '') {
+                console.error('Format specifier at end of string', format);
+            } else {
+                let formatter = specifiers[char];
+                if (formatter == undefined) {
+                    console.error('Formatter', f, 'at index', next + 1, 'is invalid', format);
+                } else {
+                    filename += formatter(downloadItem);
+                }
+            }
+        }
+    }
+
+    return filename;
+}
+
+function determineFilename(downloadItem, suggest) {
+    const format = '%y-%m-%d/%f';
+    suggest({filename: formatFilename(downloadItem, format)});
+}
+
+if (typeof chrome != 'undefined') {
+    chrome.downloads.onDeterminingFilename.addListener(determineFilename);
+} else {
+    exports.determineFilename = determineFilename;
+    exports.formatFilename = formatFilename;
+    exports.specifiers = specifiers;
+}
---
+++ test/formatFilename.js
@@ -0,0 +1,40 @@
+const expect = require('chai').expect;
+var background = require('../src/background');
+
+describe('formatFilename', function() {
+    it('should return input string if no format specifiers present', function() {
+        expect(background.formatFilename({}, 'foobar')).to.equal('foobar');
+    });
+    it('should return data both before and after format specifier', function() {
+        expect(background.formatFilename({}, 'foo%%bar')).to.equal('foo%bar');
+    });
+    it('should return % for %% specifier', function() {
+        expect(background.formatFilename({}, '%%')).to.equal('%');
+    });
+    it('should return filename for %f specifier', function() {
+        expect(background.formatFilename({filename: 'example.json'}, '%f')).to.equal('example.json');
+    });
+    it('should return year for %y specifier', function() {
+        expect(background.formatFilename({startTime: '2020-09-10T14:53:08'}, '%y')).to.equal('2020');
+    });
+    it('should return 2 digit month for %m specifier', function() {
+        expect(background.formatFilename({startTime: '2020-12-10T14:53:08'}, '%m')).to.equal('12');
+        expect(background.formatFilename({startTime: '2020-09-10T14:53:08'}, '%m')).to.equal('09');
+    });
+    it('should return 2 digit day of month for %d specifier', function() {
+        expect(background.formatFilename({startTime: '2020-09-10T14:53:08'}, '%d')).to.equal('10');
+        expect(background.formatFilename({startTime: '2020-09-01T14:53:08'}, '%d')).to.equal('01');
+    });
+    it('should return 2 digit hour for %h specifier', function() {
+        expect(background.formatFilename({startTime: '2020-09-10T14:53:08'}, '%h')).to.equal('14');
+        expect(background.formatFilename({startTime: '2020-09-10T02:53:08'}, '%h')).to.equal('02');
+    });
+    it('should return 2 digit minute for %M specifier', function() {
+        expect(background.formatFilename({startTime: '2020-09-10T14:53:08'}, '%M')).to.equal('53');
+        expect(background.formatFilename({startTime: '2020-09-10T14:03:08'}, '%M')).to.equal('03');
+    });
+    it('should return 2 digit second for %s specifier', function() {
+        expect(background.formatFilename({startTime: '2020-09-10T14:53:38'}, '%s')).to.equal('38');
+        expect(background.formatFilename({startTime: '2020-09-10T14:53:08'}, '%s')).to.equal('08');
+    });
+});
